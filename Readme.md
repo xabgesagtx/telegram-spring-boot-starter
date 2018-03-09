ebca2b6d37e1e5a68eed824fe24be7baa77dbfdb
xabgesagtx <xabgesagtx@riseup.net>
2018-03-09 10:29:57 +0100

    Introducing properties to configure webhook #3 - also lombok and latest spring boot 1.5.x release

--- Readme.md
+++ Readme.md
@@ -35,8 +35,29 @@ public class Bot extends TelegramLongPollingBot {
 ```
 
 The bot will then be registered for you automatically on startup.
- 
 
-# Example
+## Configuration
+ 
+The following properties can be configured:
+
+| property | description |
+| -------- | ----------- |
+| telegram.external-url | external base url for the webhook |
+| telegram.internal-url | internal base url for the webhook |
+| telegram.key-store | keystore for the server |
+| telegram.key-store-password | keystore password for the server |
+| telegram.path-to-certificate | full path for .pem public certificate keys |
+
+You need to configure at least `external-url` and `internal-url` for webhook support. This configuration leaves the HTTPS handling to a proxy.
+
+If you configure `key-store` for your HTTPS webhook bot, you need to configure `key-store-password` as well.
+
+Also, `path-to-certificate` will only be used, if you configure the keystore.
+
+In a regular spring boot setting these properties can be set in your `application.properties` or `application.yml`.
+
+For more information on how these configuration options work, please refer to the [TelegramBots Java API](https://github.com/rubenlagus/TelegramBots).
+
+## Example
 
 An [implementation example](https://github.com/xabgesagtx/telegram-spring-boot-starter-example) is available too.
--- src/main/java/com/github/xabgesagtx/bots/TelegramBotAutoConfiguration.java
+++ src/main/java/com/github/xabgesagtx/bots/TelegramBotAutoConfiguration.java
@@ -1,29 +1,36 @@
 package com.github.xabgesagtx.bots;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.generics.BotSession;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Auto configuration for telegram bots to automatically start bots that are configured as beans.
+ */
 @Configuration
+@ConfigurationProperties("telegram")
+@Slf4j
 @ConditionalOnBean({TelegramLongPollingBot.class, TelegramWebhookBot.class})
 public class TelegramBotAutoConfiguration {
 
-	private final Logger logger = LoggerFactory.getLogger(TelegramBotAutoConfiguration.class);
-	
 	private List<BotSession> sessions = new ArrayList<>();
 	
 	@Autowired(required=false)
@@ -31,33 +38,81 @@ public class TelegramBotAutoConfiguration {
 	
 	@Autowired(required=false)
 	private List<TelegramWebhookBot> webHookBots = new ArrayList<>();
+
+	@Getter
+	@Setter
+	private String externalUrl = null;
+
+	@Getter
+	@Setter
+	private String internalUrl = null;
+
+	@Getter
+	@Setter
+	private String keyStore = null;
+
+	@Getter
+	@Setter
+	private String keyStorePassword;
+
+	@Getter
+	@Setter
+	private String pathToCertificate;
 	
 	static {
 		ApiContextInitializer.init();
 	}
 	
 	@PostConstruct
-	public void start() {
-		logger.info("Starting auto config for telegram bots");
-		TelegramBotsApi api = new TelegramBotsApi();
+	public void start() throws TelegramApiRequestException {
+		log.info("Starting auto config for telegram bots");
+		TelegramBotsApi api = getApi();
 		pollingBots.forEach(bot -> {
 			try {
-				logger.info("Registering polling bot: {}", bot.getBotUsername());
+				log.info("Registering polling bot: {}", bot.getBotUsername());
 				sessions.add(api.registerBot(bot));
 			} catch (TelegramApiException e) {
-				logger.error("Failed to register bot {} due to error {}", bot.getBotUsername(), e.getMessage());
+				log.error("Failed to register bot {} due to error {}", bot.getBotUsername(), e.getMessage());
 			}			
 		});
 		webHookBots.forEach(bot -> {
 			try {
-				logger.info("Registering web hook bot: {}", bot.getBotUsername());
+				log.info("Registering web hook bot: {}", bot.getBotUsername());
 				api.registerBot(bot);
 			} catch (TelegramApiException e) {
-				logger.error("Failed to register bot {} due to error {}", bot.getBotUsername(), e.getMessage());
+				log.error("Failed to register bot {} due to error {}", bot.getBotUsername(), e.getMessage());
 			}			
 		});
 	}
-	
+
+	/**
+	 * Get API object depending on configured properties.
+	 * If no properties are configured, the API object won't support webhooks.
+	 * @return api object
+	 * @throws TelegramApiRequestException
+	 */
+	private TelegramBotsApi getApi() throws TelegramApiRequestException {
+		TelegramBotsApi result;
+		if (!StringUtils.isEmpty(externalUrl) && !StringUtils.isEmpty(internalUrl)) {
+			if (!StringUtils.isEmpty(keyStore) && !StringUtils.isEmpty(keyStorePassword)) {
+				if (!StringUtils.isEmpty(pathToCertificate)) {
+					log.info("Initializing API with webhook support and configured keystore and path to certificate");
+					result = new TelegramBotsApi(keyStore, keyStorePassword, externalUrl, internalUrl, pathToCertificate);
+				} else {
+					log.info("Initializing API with webhook support and configured keystore");
+					result = new TelegramBotsApi(keyStore, keyStorePassword, externalUrl, internalUrl);
+				}
+			} else {
+				log.info("Initializing API with webhook support");
+				result = new TelegramBotsApi(externalUrl, internalUrl);
+			}
+		} else {
+			log.info("Initializing API without webhook support");
+			result = new TelegramBotsApi();
+		}
+		return result;
+	}
+
 	@PreDestroy
 	public void stop() {
 		sessions.stream().forEach(session -> {
